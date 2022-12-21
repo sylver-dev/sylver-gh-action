bd4d3310077f93cfc207bcd51bd13819030b3996
Geoffrey Copin <copin.geoffrey@gmail.com>
2022-12-21 11:20:59 +0100

    Fix sylver binary path

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s https://www.sylver.dev/install.sh | bash && sylver check --upload --token $1
+curl -s https://www.sylver.dev/install.sh | bash && ~/.sylver/bin/sylver check --upload --token $1
